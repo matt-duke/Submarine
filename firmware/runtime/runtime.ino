3c72d945fc302a370048eb4dd8a81bbe4ba872b6
Matt <m.duke@queensu.ca>
2019-05-10 09:30:37 -0400

    Changed structure
    Added folder for router, shares common apps

--- firmware/runtime/runtime.ino
+++ firmware/runtime/runtime.ino
@@ -1,7 +1,19 @@
 #include <EventManager.h>
+#include <EEPROM.h>
+
+
+union CRC {
+    byte length = 4
+    byte array[length];
+    uint32_t dec;
+}
+crc CRC;
 
 void setup()
 {
+    for (i=0;i<crc.length;i++;) {
+        crc.array[i] = EEPROM.read(i)
+    }
 }
 
 void loop()
