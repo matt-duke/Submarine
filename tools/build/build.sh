ac59b3bb117296cc3d0442afa51fe35a07f55ca7
Matt-Duke <m.duke@queensu.ca>
2019-06-14 22:51:52 -0400

    Auto-build test

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 cd $WORKSPACE
+
+mkdir image
+
 python3 -m pip install virtualenv 
 python3 -m virtualenv venv
 ./venv/bin/pip install -r ./tools/build/requirements.txt
 
-make ./firmware/runtime/runtime.ino
-make ./firmware/update/update.ino
+#make -C ./firmware/runtime/runtime.ino
+make -C firmware/update/update.ino
+cp firmware/update/build-mega2560/update.hex image/update.hex
 
-tar -czf image_$BUILD_NUMBER.tar.gz *
+tar -czf image_$BUILD_NUMBER.tar.gz image/*
