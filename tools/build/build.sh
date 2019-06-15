fd19fdbb9c07762374b7873ae16a26141b503199
Matt-Duke <m.duke@queensu.ca>
2019-06-14 22:59:13 -0400

    Update build.sh

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -9,7 +9,7 @@ python3 -m virtualenv venv
 ./venv/bin/pip install -r ./tools/build/requirements.txt
 
 #make -C ./firmware/runtime/runtime.ino
-make -C firmware/update/update.ino
-cp firmware/update/build-mega2560/update.hex image/update.hex
+make -C ./firmware/update
+cp ./firmware/update/build-mega2560/update.hex ./image/update.hex
 
-tar -czf image_$BUILD_NUMBER.tar.gz image/*
+tar -czf image_$BUILD_NUMBER.tar.gz ./image/*
